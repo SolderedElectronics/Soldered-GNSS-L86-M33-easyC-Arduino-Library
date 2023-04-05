aecf6c32fcf406648f8890a9578008764822912e
Docs Robot <robot@example.com>
2023-04-05 13:12:27 +0000

    Committing documentation changes

--- docs/search/functions_0.js
+++ docs/search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['begin_0',['begin',['../class_easy_c.html#a1b5a82b8a59daea79de76e496457e176',1,'EasyC::begin()'],['../class_easy_c.html#a9e71467da6f38cbe47b10e96694a4f91',1,'EasyC::begin(uint8_t _address)']]]
+  ['begin_0',['begin',['../class_easy_c.html#ac12c80d433dfbb0098a11d47e37aca34',1,'EasyC::begin()'],['../class_easy_c.html#ac414fcd9b1fa0f75b5ac17c0791ad674',1,'EasyC::begin(uint8_t _address)']]]
 ];
