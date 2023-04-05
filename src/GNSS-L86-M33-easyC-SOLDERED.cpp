2470d5ebd5aa4be15dec4792ff35ac013c63e1f9
Main Push Robot <robot@example.com>
2023-04-05 12:39:39 +0000

    Committing dev changes

--- src/GNSS-L86-M33-easyC-SOLDERED.cpp
+++ src/GNSS-L86-M33-easyC-SOLDERED.cpp
@@ -172,3 +172,51 @@ float GNSS_easyC::getAltitude()
     float altitude = atof(buf);
     return altitude;
 }
+
+/**
+ * @brief                   Get if the GPS module has sucessfuly decoded some data
+ *
+ * @returns                 bool true if yes, false if no
+ */
+bool GNSS_easyC::GNSSAvailable()
+{
+    char buf [1];
+    sendAddress(IS_AVAILABLE);
+    readData(buf, 1);
+    if(buf[0] == '1') return true;
+    else return false;
+}
+
+/**
+ * @brief                   Turn Multi Tone AIC (Active Interference Cancellation) on or off
+ * 
+ * @param                   _b True if it's to be turned on, false to turn off
+ *
+ * @returns                 None
+ */
+void GNSS_easyC::setMultiToneAIC(bool _b)
+{
+    char buf [1];
+    if(_b)
+    {
+        sendAddress(MULTI_TONE_AIC);
+    }
+    else
+    {
+        sendAddress(MULTI_TONE_AIC);
+    }
+    
+}
+
+/**
+ * @brief                   Turn AlwaysLocateTM mode on or off
+ * 
+ * @param                   _b True if it's to be turned on, false to turn off
+ *
+ * @returns                 None
+ */
+void GNSS_easyC::setAlwaysLocate(bool _b)
+{
+    char buf [1];
+    sendAddress(ALWAYS_LOCATE);
+}
